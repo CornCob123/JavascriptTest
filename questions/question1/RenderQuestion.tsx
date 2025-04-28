a3d95c0e76ef033333b81f2fe79c1970394d11c9
CornCob123 <leonsiawl@gmail.com>
2025-04-28 09:33:09 +0800

    next commit

--- questions/question1/RenderQuestion.tsx
+++ questions/question1/RenderQuestion.tsx
@@ -1,19 +1,36 @@
 import React from 'react'
 import {compose} from './Compose.ts'
 import { createRateLimitedScheduler } from './Limiting.ts';
-
-
+import { validateRequiredFields } from './RequiredKeys.ts';
+interface User {
+    id: string;
+    name: string;
+    email: string;
+    age?: number;  // Optional field
+    phone?: string; // Optional field
+  }
+  
+const invalidUser: Partial<User> = {
+    id: '456',
+    // name is missing
+    email: undefined
+  };
+  
 function RenderQuestion(){
 const add5 = (x: number): number => x + 5;
 const multiply3 = (x :number):number => x * 3;
 const square = (x: number):number => x * x;
     return(
     <>
-        <h2>Question 1.2</h2>
+        <h2>Question Required Keys</h2>
         <div style={{paddingTop:'10px'}}> 
-            <button className="btn" onClick={() => {compose(add5,multiply3,square)}} >Question 1.2 Compose</button>
+            <button className="btn" onClick={() => {validateRequiredFields(invalidUser)}} >Question Required Keys console.log states</button>
         </div>
-        <h2>Question 1.3</h2>
+        <h2>Question Compose</h2>
+        <div style={{paddingTop:'10px'}}> 
+            <button className="btn" onClick={() => {compose(add5,multiply3,square)}} >Question Compose</button>
+        </div>
+        <h2>Question Limiting</h2>
         <div style={{paddingTop:'10px'}}> 
             <button className="btn" onClick={() => {createRateLimitedScheduler(()=>{console.log('resolved')} , 4 , 3000)}} >Question 1.3 click to block view in console log</button>
         </div>
--- questions/question1/RequiredKeys.ts
+++ questions/question1/RequiredKeys.ts
@@ -2,12 +2,14 @@ type RequiredKeys<T> = {
   [K in keyof T]-?: {} extends Pick<T, K> ? never : K
 }[keyof T];
 
-function validateRequiredFields<T>(obj: Partial<T>) {
+export function validateRequiredFields<T>(obj: Partial<T>) {
   const requiredKeys = Object.keys(obj).filter(key => 
     obj[key as keyof T] === undefined || obj[key as keyof T] === null
   ) as Array<RequiredKeys<T>>;
   
   if (requiredKeys.length > 0) {
-    throw new Error(`Missing required fields: ${requiredKeys.join(', ')}`);
+    throw new Error(`Fields not defined: ${requiredKeys.join(', ')}`);
   }
 }
+
+export default validateRequiredFields
